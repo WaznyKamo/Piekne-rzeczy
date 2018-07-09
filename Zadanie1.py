63237680f6f1590ccf100e6dace98e003f625124
Kamil Ważny <waznykamil@wp.pl>
2018-07-09 19:19:28 +0200

    źle się wcześniej zapisało

---
+++ Zadanie1.py
@@ -0,0 +1,5 @@
+#Zadanie1:
+print("Przeliczanie stopni Celsjusza na Fahrenheita")
+stopnie_C = int (input('Podaj wartość w stopniach Celsjusza: '))
+stopnie_F = 9/5 * stopnie_C + 32
+print ("9/5 * ", stopnie_C, " + 32= ", stopnie_F, "\n Wartość w stopniach Fahrenheita: ", stopnie_F)
