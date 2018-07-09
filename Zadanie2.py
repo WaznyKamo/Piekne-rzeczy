5138fee6ba5088223956be217d14eb6a24cf7bca
Kamil Ważny <waznykamil@wp.pl>
2018-07-09 18:55:07 +0200

    komentarz2

---
+++ Zadanie2.py
@@ -0,0 +1,5 @@
+print("Przeliczanie stopni Fahrenheita na stopnie Celsjusza")
+stopnie_F = int(input("Podaj wartość w stopniach Fahrenheita:"))
+stopnie_C = 5 / 9 * stopnie_F - 32
+print("5/9 * %s - 32= %s" %(stopnie_F, stopnie_C))
+print("Wartość w stopniach Celsjusza to %s" % stopnie_C)
