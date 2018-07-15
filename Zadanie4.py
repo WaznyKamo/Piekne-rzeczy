34af42d707903805ef40c45511cf9a613767f2f1
Kamil Ważny <waznykamil@wp.pl>
2018-07-15 22:37:08 +0200

    Jest ok

--- Zadanie4.py
+++ Zadanie4.py
@@ -1,5 +1,18 @@
-#4. Napisz program, który poda pierwszą i ostatnią cyfrę podanej liczby
-liczba=input("Podaj liczbę: ")
-liczba_znakow=len(liczba)
-print("Pierwsza cyfra to: ", liczba[0])
-print("Ostatnia cyfra to: ", liczba[liczba_znakow-1])
+#4) Kalkulator do wyliczania wieku psa.
+#   Przez pierwsze dwa lata, każdy psi rok to 10,5 ludzkiego roku, przez reszte lat psi rok to 4 ludzkie lata
+#   Np: 15 ludzkich lat to 73 psie lata
+
+wartosc_podana = (input("Podaj wiek psa w ludzkich latach: "))
+ludzki_wiek = int(wartosc_podana)
+psi_wiek = 0
+
+if ludzki_wiek >= 2:
+    psi_wiek = 21
+    ludzki_wiek = ludzki_wiek - 2
+elif ludzki_wiek >= 1:
+    psi_wiek = 10.5
+    ludzki_wiek = ludzki_wiek - 1
+
+psi_wiek = psi_wiek + 4 * ludzki_wiek
+
+print(("{} w ludzkich latach to inaczej {} w psich latach").format(wartosc_podana, psi_wiek))
